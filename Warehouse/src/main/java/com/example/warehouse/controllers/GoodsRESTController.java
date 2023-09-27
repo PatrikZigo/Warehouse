ea2f2288a7e55f7aea1194a8a42aa050daa87a7a
Patrik Zigo <patrikzigo55@gmail.com>
2023-09-27 12:29:41 +0200

    finishing rest controller for goods

--- Warehouse/src/main/java/com/example/warehouse/controllers/GoodsRESTController.java
+++ Warehouse/src/main/java/com/example/warehouse/controllers/GoodsRESTController.java
@@ -1,10 +1,39 @@
 package com.example.warehouse.controllers;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.warehouse.DTOs.GoodsRequestDTO;
+import com.example.warehouse.services.GoodsServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-
+@RestControllerAdvice
+@RequestMapping("/api/goods")
 public class GoodsRESTController {
 
+    public final GoodsServices goodsServices;
+
+    public GoodsRESTController(GoodsServices goodsServices) {
+        this.goodsServices = goodsServices;
+    }
+
+    @PostMapping("/add-new-goods")
+    ResponseEntity<?> addNewGood(GoodsRequestDTO goodsRequestDTO){
+        return ResponseEntity.ok().body(goodsServices.addGoods(goodsRequestDTO));
+    }
+
+    @DeleteMapping("/remove-goods/{id}")
+    ResponseEntity<?> removeGoods(Long id){
+        return ResponseEntity.ok().body(goodsServices.removeGoodsById(id));
+    }
+
+    @PatchMapping("/modify-goods/{id}")
+    ResponseEntity<?> modifyGoods(Long id, GoodsRequestDTO goodsRequestDTO){
+        return ResponseEntity.ok().body(goodsServices.modifyGoodsById(id, goodsRequestDTO));
+    }
+
+    @PatchMapping("/modify-amount-of-goods/{id}/{amount}")
+    ResponseEntity<?> modifyAmountOfGoods(Long id, int amount){
+        return ResponseEntity.ok().body(goodsServices.modifyAmountOfGoodsById(id, amount));
+    }
 }
