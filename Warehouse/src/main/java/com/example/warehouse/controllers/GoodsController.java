650ef8af9612a8c2ff02e2e41558cc13eb9bc2a9
Patrik Zigo <patrikzigo55@gmail.com>
2023-09-27 12:02:33 +0200

    Add goods controller and start working on goods rest controller

--- Warehouse/src/main/java/com/example/warehouse/controllers/GoodsController.java
+++ Warehouse/src/main/java/com/example/warehouse/controllers/GoodsController.java
@@ -1,26 +1,23 @@
 package com.example.warehouse.controllers;
 
-import com.example.warehouse.DTOs.GoodsRequestDTO;
-import com.example.warehouse.models.Goods;
 import com.example.warehouse.services.GoodsServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/warehouse")
 public class GoodsController {
-    private final GoodsServices goodsServices;
+  private final GoodsServices goodsServices;
 
-    public GoodsController(GoodsServices goodsServices) {
-        this.goodsServices = goodsServices;
-    }
+  public GoodsController(GoodsServices goodsServices) {
+    this.goodsServices = goodsServices;
+  }
 
-    @PostMapping ("/add-new-goods")
-    public String addNewGoods(@ModelAttribute Goods goods, GoodsRequestDTO goodsRequestDTO){
-        model.addAttribute("")
-    }
+  @GetMapping("/all-items")
+  public String addNewGoods(Model model) {
+    model.addAttribute("allGoods", goodsServices.getAllGoods());
+    return "main";
+  }
 }
-
